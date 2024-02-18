9cbd8a5f82e1a18c8961fcb1003aaea7b94d5dd5
Bertin Ndayishimiye <neddybertin@gmail.com>
2024-02-18 14:52:04 +0100

    added social icons in footer restyled both navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,33 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { 
+    FaXTwitter,
+    FaSquareInstagram,
+    FaTiktok,
+    FaFacebook
+} 
+    from "react-icons/fa6";
+import { IconContext } from "react-icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Footer() {
-  return (
+return (
     <footer>
-    <div className="flex justify-between bg-[#06b6d4] p-10">
-      <Image src="next.svg" width="170" height="170" alt="logo"/>
-      <button>Call us</button>
+    <div className="flex justify-between items-center bg-[#dae7f4] p-10">
+    <Image src="next.svg" width="170" height="170" alt="logo"/>
+    <p>@2024 Copyright reserved</p>
+    <div className="flex gap-6">
+    <IconContext.Provider value={{ className: "shared-class cursor-pointer", size: 30 }}>
+    <>
+        <FaXTwitter />
+        <FaSquareInstagram />
+        <FaTiktok  />
+        <FaFacebook />
+    </>
+</IconContext.Provider>
+    </div>
     </div>
     </footer>
-  );
+);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Navbar() {
   return (
     <main>
-    <header className="flex justify-between bg-[#06b6d4] p-10">
+    <header className="flex justify-between bg-[#dae7f4] p-8">
       <Image src="next.svg" width="170" height="170" alt="logo"/>
-      <button>Call us</button>
+      <button class="h-10 px-5 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">Call Us</button>
     </header>
     </main>
   );
