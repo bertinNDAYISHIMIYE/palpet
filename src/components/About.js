e76492cefa631cd33d135339dbe3500e896d85e3
Bertin Ndayishimiye <neddybertin@gmail.com>
2024-02-23 16:38:14 +0100

    about component start map section

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+
+export default function About() {
+  return (
+    <div className="flex flex-col justify-center items-center m-auto md:h-[540px] md:flex-row  gap-5">
+      <div >
+        <img height={900} width={900} src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nc3xlbnwwfHwwfHx8MA%3D%3D" alt="png" />
+      </div>
+      <div>
+        <h2 className="text-3xl py-3">About Us</h2>
+        <p>
+        Welcome to Cute Pets, Hungary&apos;s premier destination for all your pet needs. With a passion for animals and a commitment to exceptional customer service, we strive to provide the best products and services for your beloved pets.
+
+At Cute Pets, we understand that pets are an important part of your family.
+<br /> <br />
+ That&apos;s why we offer a wide range of high-quality pet supplies, including food, toys, accessories, and more. 
+ Our knowledgeable staff is always ready to assist you in finding the perfect products to keep your furry friends happy and healthy. 
+ Visit us today and experience the Cute Pets difference!
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import Banner from "@/components/Banner";
 import Services from "@/components/Services";
 import Gallery from "@/components/Gallery";
+import About from "@/components/About";
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,23 +15,20 @@ export default function Home() {
     <Banner />
     <Services />
     <Gallery />
-    <div className="flex flex-col justify-center items-center m-auto md:h-[540px] md:flex-row  gap-5">
-      <div >
-        <img height={900} width={900} src="https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nc3xlbnwwfHwwfHx8MA%3D%3D" alt="png" />
-      </div>
-      <div>
-        <h2 className="text-3xl py-3">About Us</h2>
-        <p>
-        Welcome to Cute Pets, Hungary&apos;s premier destination for all your pet needs. With a passion for animals and a commitment to exceptional customer service, we strive to provide the best products and services for your beloved pets.
-
-At Cute Pets, we understand that pets are an important part of your family.
-<br /> <br />
- That&apos;s why we offer a wide range of high-quality pet supplies, including food, toys, accessories, and more. 
- Our knowledgeable staff is always ready to assist you in finding the perfect products to keep your furry friends happy and healthy. 
- Visit us today and experience the Cute Pets difference!
-        </p>
-      </div>
-    </div>
+   <About />
+   <>
+   <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+   </>
     </main>
   );
 }
